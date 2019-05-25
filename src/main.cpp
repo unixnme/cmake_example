b59982d4b6fed1147609265d1c4e61505c31771e
Unix&Me <unixnme@gmail.com>
2019-05-24 22:38:25 -0700

    works well

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,20 @@ typedef std::vector<int> Element;
 //PYBIND11_MAKE_OPAQUE(Element);
 //PYBIND11_MAKE_OPAQUE(std::vector<Element>);
 
-std::unique_ptr<std::vector<Element>> get(int i) {
-    std::unique_ptr<std::vector<Element>> ptr(new std::vector<Element>());
-    Element e;
-    ptr->emplace_back(e);
-    return ptr;
+std::unique_ptr<std::vector<Element>> get(int i, int j) {
+    auto result = std::unique_ptr<std::vector<Element>>(new std::vector<Element>());
+    for (int x=0; x<i; x++) {
+        Element e;
+        for (int y=0; y<j; y++) e.emplace_back(y);
+        result->emplace_back(e);
+        }
+    return result;
 }
-
 namespace py = pybind11;
 
 PYBIND11_MODULE(cmake_example, m) {
-    py::bind_vector<Element>(m, "Element");
+    py::bind_vector<Element>(m, "VectorInt");
+    py::bind_vector<std::vector<Element>>(m, "VectorVectorInt");
     m.def("get", &get);
 
 #ifdef VERSION_INFO
